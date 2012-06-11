2c3febc26aed96246171e804846ace68e0ac4a9e
T.Ishibashi <tksh674@yahoo.co.jp>
2012-06-12 00:10:17 +0900

    rendering circle, panel and line (sphere).

--- circle/circle/js/main.js
+++ circle/circle/js/main.js
@@ -16,9 +16,10 @@ function initThree() {
 	width = document.getElementById('canvas-frame').clientWidth;
 	height = document.getElementById('canvas-frame').clientHeight;  
 	renderer = new THREE.WebGLRenderer({antialias: true});
+	//renderer = new THREE.CanvasRenderer();
 	renderer.setSize(width, height );
 	document.getElementById('canvas-frame').appendChild(renderer.domElement);
-	renderer.setClearColorHex(0xFFFFFF, 1.0);
+	//renderer.setClearColorHex(0xFFFFFF, 1.0);
 
 
 
@@ -56,7 +57,9 @@ function initScene() {
 	//fog
 	//scene.fog = new THREE.FogExp2( 0xff0000, 0.0003 );
 	//scene.fog.color.setHSV( 0.1, 0.10, 1 );
-  	//scene.add(camera);
+	
+	
+  	scene.add(camera);
   
 }
 
@@ -68,6 +71,9 @@ function initLight() {
   light = new THREE.DirectionalLight(0xFFFFFF, 1.0, 0);
   light.position.set( 500, 500, 200 );
   scene.add(light);
+  light2 = new THREE.DirectionalLight(0xFFFFFF, 1.0, 0);
+  light2.position.set( -500, -500, -200 );
+  scene.add(light2);
 }
 
 
@@ -95,7 +101,7 @@ function initObject(){
 		
 		scene.add( line );
 
-		var line = new THREE.Line( geometry, material );
+		line = new THREE.Line( geometry, material );
 		line.position.x = ( i * 100 ) - 2000;
 		line.rotation.y = 90 * Math.PI / 180;
 		//line.rotation.x = 90 * Math.PI / 180;
@@ -104,11 +110,10 @@ function initObject(){
 	}
 
 
-
-
+/*************************ちょっとコメントアウト********************************
   for(var i=0; i<10; i++){
     circle[i] = new THREE.Mesh(
-         new THREE.CylinderGeometry(100, 100, 0, 50, 50, false),                //形状の設定
+         new THREE.CylinderGeometry(100, 100, 0, 50, 50, false),	//形状の設定
          new THREE.MeshLambertMaterial({
          	//map: new THREE.Texture( generateSprite() ),
          	//blending: THREE.AdditiveBlending,
@@ -122,9 +127,222 @@ function initObject(){
     circle[i].position.set(0,-200,1000 - 250 * i);
     circle[i].rotation.set(angle,angle,angle);
   }
+*******************************************************************************/
+
+/*
+  for(var i = 0; i < 30; i++){
+		renderCircle(1000 * Math.random() - 500 , 1000 * Math.random() - 500 , 1000 * Math.random() - 500);
+	}
+	
+*/
+
+	renderCircle(0,0,0);
+	renderCircle(-300,-300,-300);
+	renderLine(0,0,0,-300,-300,-300);
+
+
+
+  //renderLine(/*0,0,0,0,0,0*/);
+  
+  
+  //circleとpanelをレンダリングする関数
+  function renderCircle(x,y,z){
+	var circle = new THREE.Mesh(
+		new THREE.CylinderGeometry(100, 100, 0, 50, 50, false),		//形状の設定
+		new THREE.MeshLambertMaterial({
+         	//map: new THREE.Texture( generateSprite() ),
+         	//blending: THREE.AdditiveBlending,
+         	color: Math.random() * 0xffffff,
+         	opacity: 0.3,
+			
+         	}) //材質の設定
+	);
+	
+	var panel = new THREE.Mesh(
+		new THREE.PlaneGeometry(100, 80),	//形状の設定
+		new THREE.MeshLambertMaterial({
+			color: Math.random() * 0xffffff,
+			opacity: 0.3,
+			
+			}) //材質の設定
+	);
+	
+	
+	scene.add(circle);
+	scene.add(panel);
+	circle.position.set(x,y,z);
+	panel.position.set(x, y + 70, z);
+	circle.rotation.set(angle,angle,angle);
+	panel.rotation.set(0,angle,angle);
+  }
+  
+  
+  
+  //Line(Sphere)をレンダリングする関数
+  function renderLine(x0,y0,z0,x1,y1,z1){//2つの位置はcircleの位置
+	  
+	  var xx0, zz0, xx1, zz1;
+	  
+	  calcLinePos();
+	  
+	  //var distance = Math.sqrt(Math.pow(xx0 - xx1, 2) + Math.pow(zz0 - zz1, 2));
+	  var sphere_num = 10;
+	  
+	  for (var i = 0; i < sphere_num; i++){
+		  
+		
+		  
+	  	var sphere = new THREE.Mesh(
+			new THREE.SphereGeometry(10,20,20),
+			new THREE.MeshLambertMaterial({color: 0x000000})	
+		);
+	  
+	  	sphere.position.set(
+			xx0 + ( xx1 - xx0 ) / ( sphere_num - 1 ) * i,
+			y0  + ( y1  - y0  ) / ( sphere_num - 1 ) * i,
+			zz0 + ( zz1 - zz0 ) / ( sphere_num - 1 ) * i
+			);
+	  	scene.add(sphere);
+	  }
+	  
+	  
+	  
+	  //2点のcirlteの位置からLineの先端部の位置を計算する関数
+	  function calcLinePos(){
+		var dis = 60;//横移動距離
+		  
+		if(x0 > x1){
+			xx0 = x0 - dis;
+			xx1 = x1 + dis;
+		} else {
+			xx0 = x0 + dis;
+			xx1 = x1 - dis;	
+		}
+		
+		if(z0 > z1){
+			zz0 = z0 - dis;
+			zz1 = z1 + dis;
+		} else {
+			zz0 = z0 + dis;
+			zz1 = z1 - dis;	
+		}
+		
+		
+		
+	  }
+	  
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+/****************************************************************************** 
+  //Line(particle)をレンダリングする関数
+  function renderLine(x1,y1,z1,x2,y2,z2){//引数は2つのcircleの位置
+  
+  
+    //var material = new THREE.ParticleBasicMaterial( { map: new THREE.Texture( generateSprite() ), blending: THREE.AdditiveBlending } );
+
+	var PI2 = Math.PI * 2;
+	var material = new THREE.ParticleCanvasMaterial( {//particleの材質
+
+		color: 0xff00ff,
+		program: function ( context ) {
+
+			context.beginPath();
+			context.arc( 0, 0, 10, 0, PI2, true );
+			context.fill();
+
+		}
+
+	} );
+  
+    var particle = new THREE.Particle( material );
+    
+    //initParticle(particle,500);
+    
+    
+    particle.position.set(0,0,0);
+    //particle.position.x = 0;
+    //particle.position.z = 0;
+    //particle.scale.x = particle.scale.y = 4;
+	scene.add(particle);
+  
+  }
+  
+******************************************************************************/
+  
+  
+  
+  
+/*******************************************************************************
+
+  function generateSprite() {
+
+	var canvas = document.createElement( 'canvas' );
+	canvas.width = 16;
+	canvas.height = 16;
+
+	var context = canvas.getContext( '2d' );
+	var gradient = context.createRadialGradient( canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2 );
+	gradient.addColorStop( 0, 'rgba(255,255,255,1)' );
+	gradient.addColorStop( 0.2, 'rgba(0,255,255,1)' );
+	gradient.addColorStop( 0.4, 'rgba(0,0,64,1)' );
+	gradient.addColorStop( 1, 'rgba(0,0,0,1)' );
+
+	context.fillStyle = gradient;
+	context.fillRect( 0, 0, canvas.width, canvas.height );
+
+	return canvas;
+
+  };
+  
+
+  function initParticle( particle, delay ) {
+
+	var particle = this instanceof THREE.Particle ? this : particle;
+	var delay = delay !== undefined ? delay : 0;
+
+	particle.position.x = 0;
+	particle.position.y = 0;
+	particle.position.z = 0;
+	particle.scale.x = particle.scale.y = Math.random() * 3 + 1;
+
+	new TWEEN.Tween( particle )
+		.delay( delay )
+		.to( {}, 10000 )
+		.onComplete( initParticle )
+		.start();
+
+	new TWEEN.Tween( particle.position )
+		.delay( delay )
+		.to( { x: Math.random() * 4000 - 2000, y: Math.random() * 1000 - 500, z: Math.random() * 4000 - 2000 }, 10000 )
+		.start();
+
+	new TWEEN.Tween( particle.scale )
+		.delay( delay )
+	.to( { x: 0, y: 0 }, 10000 )
+	.start();
+
+  }
+*******************************************************************************/
+
 }
-
-
 
 
 
@@ -159,26 +377,6 @@ function onDocumentTouchMove( event ) {
 }
 
 
-
-
-function generateSprite() {
-
-	var canvas = document.createElement( 'canvas' );
-	canvas.width = 16;
-	canvas.height = 16;
-
-	var context = canvas.getContext( '2d' );
-	var gradient = context.createRadialGradient( canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2 );
-	gradient.addColorStop( 0, 'rgba(255,255,255,1)' );
-	gradient.addColorStop( 0.2, 'rgba(0,255,255,1)' );
-	gradient.addColorStop( 0.4, 'rgba(0,0,64,1)' );
-	gradient.addColorStop( 1, 'rgba(0,0,0,1)' );
-
-	context.fillStyle = gradient;
-	context.fillRect( 0, 0, canvas.width, canvas.height );
-
-	return canvas;
-}
 
 
 
