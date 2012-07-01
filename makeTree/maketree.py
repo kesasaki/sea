e219ec423897044a318c378b3a545d067a6f4270
Lewuathe <sasaki_kai@lewuathe.sakura.ne.jp>
2012-07-02 08:52:52 +0900

    Bookmark Data were added

--- makeTree/maketree.py
+++ makeTree/maketree.py
@@ -6,21 +6,38 @@ bookmarks = ["http://www.yahoo.co.jp",
              "http://www.google.co.jp",
              "http://www.gree.co.jp",
              "http://www.facebook.com",
-             "http://www.twitter.com",
-             "http://lewuathe.sakura.ne.jp"]
+             "http://twitter.com",
+             "http://m.twitter.com",
+             "http://mixi.co.jp",
+             "http://github.com",
+             "http://fotologue.jp"
+             "http://lewuathe.sakura.ne.jp",
+             "http://www.w3.org",
+             "http://www.yahoo.co.jp",
+             "http://k.yimg.jp",
+             "http://search.yahoo.co.jp",
+             "http://eva.yahoo.co.jp",
+             "http://shopping.yahoo.co.jp",
+             "http://auctions.yahoo.co.jp",
+             "http://travel.yahoo.co.jp",
+             "http://b.www.yahoo.co.jp",
+             "http://toolbar.yahoo.co.jp",
+             "http://dailynews.yahoo.co.jp",
+             "http://ard.yahoo.co.jp"]
 
 
-def makeTree(url,depth):
+def makeTree(url,depth,visited):
     out = {}
     childs = []
     hasChild = 0
     res = urllib.urlopen(url)
+    visited.append(url)
     urlReg = re.compile('http://[^/]*')
     match = urlReg.findall(res.read())
     if depth > 0 :
         for child in match :
-            if child in bookmarks:
-                childObject = makeTree(child,depth-1)
+            if child in bookmarks and not child in visited:
+                childObject = makeTree(child,depth-1,visited)
                 childs.append(childObject)
                 hasChild = 1
 
@@ -40,6 +57,7 @@ def makeTree(url,depth):
 if __name__ == "__main__":
     argvs = sys.argv
     length = len(argvs)
-    print makeTree(argvs[1],4)
+    visited = []
+    print makeTree(argvs[1],4,visited)
 
 
